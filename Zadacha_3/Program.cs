fa4046368c6df91ece5f1bf28ebc8f8f0e7f4577
Maria Babina <babina.mariyka@gmail.com>
2024-01-04 14:59:34 +0200

    Added Zadacha3

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+//  Использовать рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+int [] array = {1,45,76,44,98};
+PrintArrayReversedFrom (array, array.Length-1);
+
+void PrintArrayReversedFrom (int [] arr, int index)
+{
+    if(index < 0)
+    {
+        return;
+    }
+    System.Console.WriteLine (arr [index]);
+    PrintArrayReversedFrom (arr, index-1);
+
+}
